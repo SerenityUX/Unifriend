d1c81b3a15f9278d6b8e8db5b249ac7e5273b3bb
SerenityUX <thomas@serenidad.app>
2023-03-24 09:13:52 -0400

    Initial Commit

---
+++ Unifriend/UnifriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnifriendApp.swift
+//  Unifriend
+//
+//  Created by Thomas Stubblefield on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct UnifriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
